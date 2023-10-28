f8d4a16d3f1b95a41289a657ec9b8e7a58e4c410
Zheng Lin Lei <zheng9112003@gmail.com>
2023-10-28 23:55:10 +0200

    Fixed python print buffer problem

--- recollect.py
+++ recollect.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--min', help='Min. range', required=True)
     parser.add_argument('-M', '--max', help='Max. range', required=True)
+    # Add verbose option
+    parser.add_argument('-v', '--verbose', help='Verbose data', action='store_true')
     args = parser.parse_args()
 
     #!TODO: Avaliable only for DNI. NIE and PASSPORT not implemented
@@ -20,6 +22,8 @@ if __name__ == "__main__":
         r0 = login(user, password)
         
         if "ERR55" not in r0.url:
+            if args.verbose:
+                print(user)
             # User exist
             open('./users.txt', 'a').write(user + '\n')
 
