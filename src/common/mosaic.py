50d815d5715a05e2ec66a2e42bb351bf710e9be4
Zheng Lin Lei <zheng9112003@gmail.com>
2024-12-15 12:13:32 +0100

    fix: Empty files removed

--- src/common/mosaic.py
+++ src/common/mosaic.py
@@ -1,6 +1,6 @@
-import os
+import os, sys
 import math
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 def create_mosaic(image_folder, output_path):
     # Load all images from the folder
@@ -14,28 +14,32 @@ def create_mosaic(image_folder, output_path):
     grid_width = math.ceil(math.sqrt(num_images))
     grid_height = math.ceil(num_images / grid_width)
 
-    # Open images in order and determine the maximum width and height
-    images = [(Image.open(img).resize((240, 280)), img) for img in image_files]
-    # Sort by filename
-    images.sort(key=lambda x: int(os.path.basename(x[1]).split('.')[0]))
+    try:
+        # Open images in order and determine the maximum width and height, but before if the image is not empty
+        images = [(Image.open(f).resize((240, 280)), f) for f in image_files if os.path.getsize(f) > 0]
+        # Sort by filename
+        images.sort(key=lambda x: int(os.path.basename(x[1]).split('.')[0]))
 
-    img_widths = [img[0].size[0] for img in images]
-    img_heights = [img[0].size[1] for img in images]
+        img_widths = [img[0].size[0] for img in images]
+        img_heights = [img[0].size[1] for img in images]
 
-    # Use the largest image dimensions for consistent spacing
-    max_width = max(img_widths)
-    max_height = max(img_heights)
+        # Use the largest image dimensions for consistent spacing
+        max_width = max(img_widths)
+        max_height = max(img_heights)
 
-    # Create a blank canvas for the mosaic
-    mosaic_width = grid_width * max_width
-    mosaic_height = grid_height * max_height
-    mosaic = Image.new('RGB', (mosaic_width, mosaic_height), (255, 255, 255))  # White background
+        # Create a blank canvas for the mosaic
+        mosaic_width = grid_width * max_width
+        mosaic_height = grid_height * max_height
+        mosaic = Image.new('RGB', (mosaic_width, mosaic_height), (255, 255, 255))  # White background
 
-    # Paste images onto the mosaic
-    for index, (img, _) in enumerate(images):
-        x = (index % grid_width) * max_width
-        y = (index // grid_width) * max_height
-        mosaic.paste(img, (x, y))
+        # Paste images onto the mosaic
+        for index, (img, _) in enumerate(images):
+            x = (index % grid_width) * max_width
+            y = (index // grid_width) * max_height
+            mosaic.paste(img, (x, y))
 
-    # Save the mosaic
-    mosaic.save(output_path)
+        # Save the mosaic
+        mosaic.save(output_path)
+    except UnidentifiedImageError as e:
+        print(f"Error: {e}", file=sys.stderr)
+    
